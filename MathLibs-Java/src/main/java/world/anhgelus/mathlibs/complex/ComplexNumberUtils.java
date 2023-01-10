d846c63820617e3fad9c61c5baf5de07351dbcf4
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-01-10 20:28:43 +0100

    fix(complexe) fix divide and multiply

--- MathLibs-Java/src/main/java/world/anhgelus/mathlibs/complex/ComplexNumberUtils.java
+++ MathLibs-Java/src/main/java/world/anhgelus/mathlibs/complex/ComplexNumberUtils.java
@@ -13,8 +13,8 @@ public class ComplexNumberUtils {
 
     /**
      * Subtraction of Complex Number
-     * @param a Complex Number to add
-     * @param b Complex Number to add
+     * @param a Complex Number to subtract
+     * @param b Complex Number to subtract
      * @return Result of the subtraction
      */
     public static ComplexNumber sub(ComplexNumber a, ComplexNumber b) {
@@ -23,13 +23,14 @@ public class ComplexNumberUtils {
 
     /**
      * Multiplication of Complex Number
-     * @param a Complex Number to add
-     * @param b Complex Number to add
+     * @param a Complex Number to multiply
+     * @param b Complex Number to multiply
      * @return Result of the multiplication
      */
     public static ComplexNumber mul(ComplexNumber a, ComplexNumber b) {
-        /* (a+ci)(b+di)
-        * <=> a(b+di) + c(bi-d)
+        /*
+        * (a+ci)(b+di)
+        * <=> a(b+di) + ci(b+di)
         * <=> ab + adi + bci - cd
         * <=> ab - cd + (ad + bc)i
         */
@@ -39,18 +40,18 @@ public class ComplexNumberUtils {
 
     /**
      * Division of Complex Number
-     * @param a Complex Number to add
-     * @param b Complex Number to add
+     * @param a Complex Number to divide
+     * @param b Complex Number to divide
      * @return Result of the division
      */
     public static ComplexNumber div(ComplexNumber a, ComplexNumber b) {
-        /* (a+ci)/(b+di)
+        /*
+        * (a+ci)/(b+di)
         * <=> (a+ci)(b-di)/(b+di)(b-di)
-        * <=> (a+ci)(b-di)/(b^2+d^2)
-        * <=> (ab-cd)/(b^2+d^2) + (bc+ad)/(b^2+d^2)i
+        * <=> ab-adi+cib+cd/(b^2+d^2)
         */
         final double denominator = b.real * b.real + b.imaginary.number * b.imaginary.number;
-        final ComplexNumber intermediate = mul(a, b);
+        final ComplexNumber intermediate = mul(a, new ComplexNumber(b.real, -b.imaginary.number));
         return new ComplexNumber(intermediate.real / denominator,
                         intermediate.imaginary.number / denominator);
     }
