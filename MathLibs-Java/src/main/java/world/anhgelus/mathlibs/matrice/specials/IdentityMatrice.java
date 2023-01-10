f36a4f6d49993748f0f6c92189ce4e2da10c0f12
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-01-10 21:15:27 +0100

    refactor(matrice) change parent of IdentityMatrice

--- MathLibs-Java/src/main/java/world/anhgelus/mathlibs/matrice/specials/IdentityMatrice.java
+++ MathLibs-Java/src/main/java/world/anhgelus/mathlibs/matrice/specials/IdentityMatrice.java
@@ -3,9 +3,9 @@ package world.anhgelus.mathlibs.matrice.specials;
 import world.anhgelus.mathlibs.matrice.Matrice;
 import world.anhgelus.mathlibs.matrice.MatriceException;
 
-public class IdentityMatrice extends Matrice {
+public class IdentityMatrice extends SquareMatrice {
     public IdentityMatrice(int order) {
-        super(order, order);
+        super(order);
         for (int i = 0; i < order; i++) {
             try {
                 super.set(i, i, 1);
